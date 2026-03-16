42f29b06ac45f9c10e2f213047052e92f1a19c8a
Ash <ashtrindade5@gmail.com>
2026-03-16 11:01:20 -0300

    chore: added log tags to improve debugging

--- app/src/debug/kotlin/io/github/landwarderer/futon/core/util/ext/Debug.kt
+++ app/src/debug/kotlin/io/github/landwarderer/futon/core/util/ext/Debug.kt
@@ -1,8 +1,11 @@
 package io.github.landwarderer.futon.core.util.ext
 
 import android.os.Looper
+import android.util.Log
 
 fun Throwable.printStackTraceDebug() = printStackTrace()
+
+fun Throwable.printStackTraceDebug(tag: String) = Log.e(tag, this.stackTraceToString())
 
 fun assertNotInMainThread() = check(Looper.myLooper() != Looper.getMainLooper()) {
 	"Calling this from the main thread is prohibited"
--- app/src/release/kotlin/io/github/landwarderer/futon/core/util/ext/Debug.kt
+++ app/src/release/kotlin/io/github/landwarderer/futon/core/util/ext/Debug.kt
@@ -5,4 +5,5 @@ package io.github.landwarderer.futon.core.util.ext
 @Suppress("NOTHING_TO_INLINE")
 inline fun Throwable.printStackTraceDebug() = Unit
 
+inline fun Throwable.printStackTraceDebug(tag: String) = Unit
 fun assertNotInMainThread() = Unit
